21a3bcecd5045d8dd29d28fb91490a727ccff5ac
Nico Watine <nico@studioncreations.com>
2020-03-20 14:25:00 -0500

    Style paragraph

--- src/pages/index.php
+++ src/pages/index.php
@@ -11,7 +11,8 @@ body-class: index
 <section class="upcoming-events-section" style="padding-top: 72px;">
 	<div class="row">
 		<div class="column text-center">
-			<h1 class="section-headline text-uppercase font-weight-light text-red" style="font-size: 72px;">Upcoming <span class="font-weight-bold text-white">Events</span></h1>
+			<h1 class="section-headline text-uppercase font-weight-light text-red mb-16" style="font-size: 72px;">Upcoming <span class="font-weight-bold text-white">Events</span></h1>
+			<p class="paragraph font-weight-light" style="font-size: 24px; color: #808080; max-width: 800px; margin-left: auto; margin-right: auto;">If you ever have the opportunity to see Tiesto, clear your calendar, travel across a state (or three). Just do whatever you need to do. Seriously. Go to a Tiesto&nbsp;show...</p>
 		</div>
 	</div>
 </section>
