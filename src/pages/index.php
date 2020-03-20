258c4c924c7e5d09e23376622864d34f457873b3
Nico Watine <nico@studioncreations.com>
2020-03-20 14:20:53 -0500

    Style section-heading

--- src/pages/index.php
+++ src/pages/index.php
@@ -8,3 +8,10 @@ og-image:
 body-id: index
 body-class: index
 ---
+<section class="upcoming-events-section" style="padding-top: 72px;">
+	<div class="row">
+		<div class="column text-center">
+			<h1 class="section-headline text-uppercase font-weight-light text-red" style="font-size: 72px;">Upcoming <span class="font-weight-bold text-white">Events</span></h1>
+		</div>
+	</div>
+</section>
