1ae6ba29e637909c14c02d2e28a0d517348e11b3
Nico Watine <nico@studioncreations.com>
2020-03-25 12:19:10 -0500

    Correclty inline-style new section-headline

--- src/pages/index.php
+++ src/pages/index.php
@@ -11,7 +11,7 @@ body-class: index
 <section class="upcoming-events-section" style="padding-top: 72px;">
 	<div class="row">
 		<div class="column text-center">
-			<h1 class="section-headline text-uppercase font-weight-light text-red mb-16" style="font-size: 72px;">Upcoming <span class="font-weight-bold text-white">Events</span></h1>
+			<h1 class="section-headline text-uppercase text-red mb-16 ff-compacta font-weight-bold" style="font-size: 90px; letter-spacing: 1.13px;">Upcoming <span class="text-white">Events</span></h1>
 			<p class="paragraph font-weight-light" style="font-size: 24px; color: #808080; max-width: 800px; margin-left: auto; margin-right: auto; margin-bottom: 64px;">If you ever have the opportunity to see Tiesto, clear your calendar, travel across a state (or three). Just do whatever you need to do. Seriously, get to that Tiesto&nbsp;show...</p>
 		</div>
 	</div>
