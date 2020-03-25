e71d0244b72a033eaf398192bfe539c3dba346ec
Nico Watine <nico@studioncreations.com>
2020-03-25 10:04:26 -0500

    Progress

--- src/pages/index.php
+++ src/pages/index.php
@@ -12,7 +12,29 @@ body-class: index
 	<div class="row">
 		<div class="column text-center">
 			<h1 class="section-headline text-uppercase font-weight-light text-red mb-16" style="font-size: 72px;">Upcoming <span class="font-weight-bold text-white">Events</span></h1>
-			<p class="paragraph font-weight-light" style="font-size: 24px; color: #808080; max-width: 800px; margin-left: auto; margin-right: auto;">If you ever have the opportunity to see Tiesto, clear your calendar, travel across a state (or three). Just do whatever you need to do. Seriously. Go to a Tiesto&nbsp;show...</p>
+			<p class="paragraph font-weight-light" style="font-size: 24px; color: #808080; max-width: 800px; margin-left: auto; margin-right: auto; margin-bottom: 64px;">If you ever have the opportunity to see Tiesto, clear your calendar, travel across a state (or three). Just do whatever you need to do. Seriously, get to that Tiesto&nbsp;show...</p>
+		</div>
+	</div>
+	<div class="row expanded">
+		<div class="large-3 columns">
+			<h3 class="text-red club-name" style="margin-bottom: 48px; line-height: 1.2; font-size: 48px;">1015 Folsom</h3>
+			<p class="location font-weight-light" style="font-size: 36px; margin-bottom: 24px; line-height: 1.2;">San Francisco, CA</p>
+			<p class="time font-weight-light" style="font-size: 36px; margin-bottom: 24px; line-height: 1.2;">11:00 PM</p>
+		</div>
+		<div class="large-3 columns">
+			<h3 class="text-red club-name" style="margin-bottom: 48px; line-height: 1.2; font-size: 48px;">Hakkasan</h3>
+			<p class="location font-weight-light" style="font-size: 36px; margin-bottom: 24px; line-height: 1.2;">Las Vegas, NV</p>
+			<p class="time font-weight-light" style="font-size: 36px; margin-bottom: 24px; line-height: 1.2;">11:00 PM</p>
+		</div>
+		<div class="large-3 columns">
+			<h3 class="text-red club-name" style="margin-bottom: 48px; line-height: 1.2; font-size: 48px;">Omnia</h3>
+			<p class="location font-weight-light" style="font-size: 36px; margin-bottom: 24px; line-height: 1.2;">Las Vegas, NV</p>
+			<p class="time font-weight-light" style="font-size: 36px; margin-bottom: 24px; line-height: 1.2;">11:00 PM</p>
+		</div>
+		<div class="large-3 columns">
+			<h3 class="text-red club-name" style="margin-bottom: 48px; line-height: 1.2; font-size: 48px;">Île Soniq</h3>
+			<p class="location font-weight-light" style="font-size: 36px; margin-bottom: 24px; line-height: 1.2;">Montréal, QC</p>
+			<p class="time font-weight-light" style="font-size: 36px; margin-bottom: 24px; line-height: 1.2;">11:00 PM</p>
 		</div>
 	</div>
 </section>
